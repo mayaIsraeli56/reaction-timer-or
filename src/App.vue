c6f8655a70fc507380f630f8e2d7c32e3982ac97
mayaIsraeli56 <maya.isra56@gmail.com>
2023-04-02 15:12:32 +0300

    Add existing project files to Git

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <h1 class="h1">Ninja React Timer</h1>
+  <button class="btn" @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"></Block>
+  <Results v-if="showResults" :score="score" ></Results>
+  <!-- <p v-if="showResults">Reaction time: {{ score }} ms</p> -->
+</template>
+
+<script>
+import Block from './components/Block.vue'
+import Results from './components/Results.vue';
+
+export default {
+  name : 'App',
+  components : {Block, Results},
+  data () {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
+    }
+  },
+  methods : {
+    start () {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      this.showResults= false;
+    },
+    endGame (reactionTime) {
+        this.score = reactionTime;
+        this.isPlaying = false;
+        this.showResults= true;
+    },
+  }
+
+}
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Arial, Helvetica, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osk-font-smoothing: grayscale;
+    text-align: center;
+    color: #444;
+  }
+
+
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,23 @@
+<template>
+    <p>Reaction time: {{ score }} ms</p>
+</template>
+
+<script>
+export default {
+  name : 'Results',
+  props: ['score'],
+  data () {
+    return {
+
+    }
+  },
+  methods : {
+
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
