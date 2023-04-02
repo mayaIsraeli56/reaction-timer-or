d6749cb6991392ce16395c6c6c0122655d919f27
Maya Israeli <maya.isra56@gmail.com>
2023-04-02 14:07:15 +0300

    first

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <h1>Ninja React Rimer</h1>
+  <br>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay"></Block>
+</template>
+
+<script>
+import Block from './components/Block.vue'
+
+export default {
+  name : 'App',
+  components : {Block},
+  data () {
+    return {
+      isPlaying: false,
+      delay: null,
+
+    }
+  },
+  methods : {
+    start () {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+    }
+  }
+
+}
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Arial, Helvetica, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osk-font-smoothing: grayscale;
+    text-align: center;
+    color: #444;
+  }
+</style>
